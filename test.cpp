68983445f08d46d77a39f4f8e61dd38c2f77ecb3
AB-IN-lsy <liusy200708@163.com>
2021-03-08 22:11:48 +0800

    s

--- test.cpp
+++ test.cpp
@@ -1,3 +1,11 @@
+/*
+ * @Description: https://blog.csdn.net/qq_45859188
+ * @Author: NEFU AB_IN
+ * @version: 1.0
+ * @Date: 2020-12-10 10:46:04
+ * @LastEditors: NEFU AB_IN
+ * @LastEditTime: 2021-03-08 22:08:09
+ */
 #include<bits/stdc++.h>
 using namespace std;
 #define ll                          long long
@@ -18,61 +26,10 @@ typedef pair<int, int>               pii;
 typedef pair<ll, ll>                 pll;
 const int INF = 0x3f3f3f3f;
 
-namespace q_pow_mm{
-    ll mm(ll a, ll b, ll m){
-        ll res = 0;
-        while(b){
-            if(b & 1) 
-                res = (res + a) % m;
-            a = (a * 2) % m;
-            b >>= 1;
-        }
-        return res;
-    }
-    ll q (ll a, ll b){
-        ll ret = 1;
-        while(b){
-            if(b & 1)
-                ret = ret * a;
-            a = a * a;
-            b = b >> 1;
-        }
-        return ret;
-    }
-    ll qm (ll a, ll b, ll c){
-        a = a % c;
-        ll ret = 1 % c;
-        while(b){
-            if(b & 1)
-                ret = mm(ret, a, c) % c;
-            a = mm(a, a, c) % c;
-            b = b >> 1;
-        }
-        return ret;
-    }
-}
-using namespace q_pow_mm;
-
-ll get_zhishu(ll x){
-	for(int i=2;i<=x;i++){
-		if(x%i==0) return i;
-	}
-}
-
 void solve(){
-    ll n=1001733993063167141;
-	ll d=212353;
-	ll C=20190324;
-	ll p=get_zhishu(n);
-	printf("p的值为%lld\n",p);
-	ll q=n/p;
-	printf("q的值为%lld\n",q);
-	
-	ll k=(p-1)*(q-1);
-    ll e = qm(d, k - 2, k);
-    cout << qm(C, e, n);
 
 }
+
 int main()
 {
     IOS;
