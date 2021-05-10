cfb4e88f1a7fa1e4a366467fa48dc07273b31053
AB-IN-lsy <liusy200708@163.com>
2021-05-10 20:41:04 +0800

    new

--- test.cpp
+++ test.cpp
@@ -4,51 +4,30 @@
  * @version: 1.0
  * @Date: 2020-12-10 10:46:04
  * @LastEditors: NEFU AB_IN
- * @LastEditTime: 2021-05-09 16:21:07
+ * @LastEditTime: 2021-05-10 20:40:59
  */
 #include<bits/stdc++.h>
 using namespace std;
-#define ll                          long long
+#define LL                          long long
+#define ULL                         unsigned long long
 #define SZ(X)                       ((int)(X).size())
 #define MP                          make_pair
 #define IOS                         ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
 
-vector <int> ans, t;
-vector < vector<int> > v;
-int st[10010];
-int mx;
-
-void dfs(int x, int sum){
-	if(sum > mx) ans = t, mx = sum;
-	else if(sum == mx && t < ans) ans = t; 
-	for(auto& i : v[x]){
-		t.push_back(i);
-		dfs(i, sum + 1);
-		t.pop_back();
+struct cmp{
+	bool operator() (const int &a, const int &b){
+		return a > b;
 	}
-	return;
-}
-
+};
 int main()
 {
 	IOS;
-	int n;
-	cin >> n;
-	v.resize(n);
-	for(auto& vi : v){
-		int tmp;
-		cin >> tmp;
-		vector < int > vv(tmp);
-		for(auto& i : vv) cin >> i, st[i] = 1;
-		vi = vv;
-	}
-	int root = 0;
-	while(st[root]) root ++;
-	dfs(root, 1);
-	cout << mx << endl;
-	cout << root;
-	for(int j = 0; j < SZ(ans); j++){
-		cout << " "<< ans[j];
-	}
+	int a[5] = {1, 8, 3, 9, 1};
+	priority_queue <int, vector<int>, cmp> q(a, a + 5);
+	cout << q.top() << " "; q.pop();
+	cout << q.top() << " "; q.pop();
+	cout << q.top() << " "; q.pop();
+	cout << q.top() << " "; q.pop();
+	cout << q.top() << " "; q.pop();
 	return 0;
 }
--- test1.cpp
+++ test1.cpp
@@ -1,65 +1,98 @@
+/*
+ * @Description: https://blog.csdn.net/qq_45859188
+ * @Author: NEFU AB_IN
+ * @version: 1.0
+ * @Date: 2021-03-06 20:12:14
+ * @LastEditors: NEFU AB_IN
+ * @LastEditTime: 2021-05-10 20:08:49
+ */
 #include<bits/stdc++.h>
-#define SZ(X)                       ((int)(X).size())
-#define IOS                         ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
 using namespace std;
-const int N = 1e6 + 10;
+#define LL                          long long
+#define ULL                         unsigned long long
+#define SZ(X)                       ((int)(X).size())
+#define MP                          make_pair
+#define IOS                         ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
 
-int n;
-int son[N], st[N];
+namespace dijkstra{
+    const int N = 1e6 + 10;
+    const int inf = 0x3f3f3f3f;
 
-struct Edge
-{
-    int u, v, w, next;
-}edge[N << 2];
-int head[N];
-int cnt;
-void add_edge(int u, int v, int w)
-{
-    edge[cnt].u = u;
-    edge[cnt].v = v;
-    edge[cnt].w = w;
-    edge[cnt].next = head[u];
-    head[u] = cnt++;
-}
-void init(){
-    memset(head, -1, sizeof(head));
-    cnt = 0;
-}
+    struct sa{
+        int pos; // 位置
+        int dis; // 距离
+    };
 
-int dfs(int u){
-    int ans = 0;
-    son[u] = -1;
-    for(int i = head[u]; ~i; i = edge[i].next){
-        int j = edge[i].v;
-        int d = dfs(j);
-        if(ans < d) ans = d, son[u] = j;
-        else if(ans == d && j < son[u]) son[u] = j;
+    struct cmp{ // 重写函数
+        bool operator() (const sa &a, const sa &b) const{
+            return a.dis > b.dis;
+        }    
+    };
+
+    priority_queue <sa, vector<sa>, cmp> q;
+    
+    struct Edge
+    {
+        int u, v, w, next;
+    }edge[N << 2];
+    int head[N];
+    int cnt;
+    void init(){
+        memset(head, -1, sizeof(head));
+        cnt = 0;
     }
-    return ans + 1;
+    void add_edge(int u, int v, int w)
+    {
+        edge[cnt].u = u;
+        edge[cnt].v = v;
+        edge[cnt].w = w;
+        edge[cnt].next = head[u];
+        head[u] = cnt++;
+    }
+    int dis[N];
+    bool vis[N];
+    void dij(int s, int v, int d[]){
+        // 从s开始，v为s自带的初始值
+        memset(vis, 0, sizeof(vis));
+        for(int i = 1; i <= N; i++) d[i] = inf;
+        d[s] = v;
+        q.push( (sa) {s, v});
+        while(!q.empty())
+        {
+            sa ns = q.top();
+            q.pop();
+            int x = ns.pos;
+            if(vis[x]) continue;
+            vis[x] = 1;
+            for(int i = head[x]; ~i ; i = edge[i].next)
+            {
+                int to = edge[i].v;
+                int dd = d[x] + edge[i].w;
+                if(d[to] > dd)
+                {
+                    d[to] = dd;
+                    q.push( (sa) {to, d[to]});
+                }
+            }
+        }
+    }
 }
+using namespace dijkstra;
 
 int main()
 {
     IOS;
     init();
-    cin >> n;
-    for(int i = 0; i < n; i ++){
-        int tmp;
-        cin >> tmp;
-        while(tmp --){
-            int x;
-            cin >> x;
-            add_edge(i, x, 0);
-            st[x] = 1;
-        }
+    int n, m, s;
+    cin >> n >> m >> s;
+    for(int i = 1; i <= m; i ++){
+        int u, v, w;
+        cin >> u >> v >> w;
+        add_edge(u, v, w);
     }
-    int root = 0;
-    while(st[root]) root ++;
-    cout << dfs(root) << endl;
-    cout << root;
-    while(son[root] != -1){
-        cout << " " << son[root];
-        root = son[root]; 
-    }
+    dij(s, 0, dis);
+
+    for(int i = 1; i <= n; i++) cout << dis[i] << " ";
+    
     return 0;
 }
