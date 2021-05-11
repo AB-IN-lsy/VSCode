09b70bee2b2cfe95059831448e09a936a5a54c73
AB-IN-lsy <liusy200708@163.com>
2021-05-11 21:03:21 +0800

    dijstra更改

--- test1.cpp
+++ test1.cpp
@@ -1,11 +1,3 @@
-/*
- * @Description: https://blog.csdn.net/qq_45859188
- * @Author: NEFU AB_IN
- * @version: 1.0
- * @Date: 2021-03-06 20:12:14
- * @LastEditors: NEFU AB_IN
- * @LastEditTime: 2021-05-10 20:08:49
- */
 #include<bits/stdc++.h>
 using namespace std;
 #define LL                          long long
@@ -13,86 +5,66 @@ using namespace std;
 #define SZ(X)                       ((int)(X).size())
 #define MP                          make_pair
 #define IOS                         ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+typedef pair<int , int>             PII;
 
-namespace dijkstra{
-    const int N = 1e6 + 10;
-    const int inf = 0x3f3f3f3f;
-
-    struct sa{
-        int pos; // 位置
-        int dis; // 距离
-    };
-
-    struct cmp{ // 重写函数
-        bool operator() (const sa &a, const sa &b) const{
-            return a.dis > b.dis;
-        }    
-    };
-
-    priority_queue <sa, vector<sa>, cmp> q;
-    
+namespace dij{
+    const int N = 1e7 + 10;
+    const int INF = 0x3f3f3f3f;
     struct Edge
     {
-        int u, v, w, next;
+        int u, v, w, ne;
     }edge[N << 2];
-    int head[N];
-    int cnt;
-    void init(){
-        memset(head, -1, sizeof(head));
-        cnt = 0;
-    }
-    void add_edge(int u, int v, int w)
+    int h[N], cnt, dist[N], vis[N];
+
+    void add(int u, int v, int w)
     {
         edge[cnt].u = u;
         edge[cnt].v = v;
         edge[cnt].w = w;
-        edge[cnt].next = head[u];
-        head[u] = cnt++;
+        edge[cnt].ne = h[u];
+        h[u] = cnt++;
     }
-    int dis[N];
-    bool vis[N];
-    void dij(int s, int v, int d[]){
-        // 从s开始，v为s自带的初始值
-        memset(vis, 0, sizeof(vis));
-        for(int i = 1; i <= N; i++) d[i] = inf;
-        d[s] = v;
-        q.push( (sa) {s, v});
-        while(!q.empty())
+    
+    void dijkstra(int s, int d[]){
+        memset(vis, 0, sizeof vis);
+        memset(d, 0x3f, sizeof dist);
+        priority_queue <PII, vector<PII>, greater<PII>> q;
+        d[s] = 0;
+        q.push({0, s}); 
+        while(q.size())
         {
-            sa ns = q.top();
+            auto ns = q.top();
             q.pop();
-            int x = ns.pos;
+            int x = ns.second;
             if(vis[x]) continue;
             vis[x] = 1;
-            for(int i = head[x]; ~i ; i = edge[i].next)
+            for(int i = h[x]; ~i ; i = edge[i].ne)
             {
                 int to = edge[i].v;
-                int dd = d[x] + edge[i].w;
-                if(d[to] > dd)
+                if(d[to] > d[x] + edge[i].w)
                 {
-                    d[to] = dd;
-                    q.push( (sa) {to, d[to]});
+                    d[to] = d[x] + edge[i].w;
+                    q.push({d[to], to});
                 }
             }
         }
     }
 }
-using namespace dijkstra;
+using namespace dij;
 
 int main()
 {
     IOS;
-    init();
-    int n, m, s;
-    cin >> n >> m >> s;
+    memset(h, -1, sizeof h);
+    int n, m;
+    cin >> n >> m;
     for(int i = 1; i <= m; i ++){
         int u, v, w;
         cin >> u >> v >> w;
-        add_edge(u, v, w);
+        add(u, v, w);
     }
-    dij(s, 0, dis);
-
-    for(int i = 1; i <= n; i++) cout << dis[i] << " ";
-    
+    dijkstra(1, dist);
+    if(dist[n] == INF) cout << "-1" << endl;
+    else cout << dist[n] << endl;
     return 0;
 }
