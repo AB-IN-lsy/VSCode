081476993e349635b3e84fcb3c4a371d77c63336
AB-IN-lsy <liusy200708@163.com>
2021-05-25 11:18:52 +0800

    new

--- test1.cpp
+++ test1.cpp
@@ -4,17 +4,5 @@
  * @version: 1.0
  * @Date: 2021-03-06 20:12:14
  * @LastEditors: NEFU AB_IN
- * @LastEditTime: 2021-05-23 15:45:28
+ * @LastEditTime: 2021-05-25 11:18:39
  */
-#include <bits/stdc++.h>
-using namespace std;
-#define LL                    long long
-#define ULL                   unsigned long long
-#define SZ(X)                 ((int)(X).size())
-#define IOS                   ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
-typedef pair<int , int>       PII;
-
-int main(){
-  IOS;
-
-}
