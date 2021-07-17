7645332be279e33b579d049b8c1a5434c7aca11f
AB-IN-lsy <liusy200708@163.com>
2021-07-17 19:59:32 +0800

    2021.7.17

--- test.cpp
+++ test.cpp
@@ -4,5 +4,21 @@
  * @version: 1.0
  * @Date: 2020-12-10 10:46:04
  * @LastEditors: NEFU AB_IN
- * @LastEditTime: 2021-06-28 19:47:27
+ * @LastEditTime: 2021-07-17 12:33:54
  */
+#include<bits/stdc++.h>
+using namespace std;
+#define LL                          long long
+#define ULL                         unsigned long long
+#define SZ(X)                       ((int)(X).size())
+#define IOS                         ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+typedef pair<int , int>             PII;
+
+
+int main()
+{
+    for(int i = 1; i <= 50; i ++){
+        cout << i * 3 << " ";
+    }
+    return 0;
+}
