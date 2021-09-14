0116ef328b41df99da85e73e1c20ab93162bdda5
AB-IN-lsy <liusy200708@163.com>
2021-09-14 20:55:13 +0800

    2021.9.14

--- PowershellScript/gitee.ps1
+++ PowershellScript/gitee.ps1
@@ -1,7 +1,8 @@
 ﻿param($a, $b)
-echo 'Wait a second, Turnning VPN on'
 cd D:\v2rayN\
 .\v2rayN.exe
+echo 'Wait a second, Turnning VPN on'
+Start-Sleep -s 1
 echo 'Successful Turning'
 $b = Get-Date -Format 'yyyy.M.d'
 echo "Commit info = $b"
