8f23ac9359800738aa5bcff86f3b319c4d10eb5a
AB-IN-lsy <liusy200708@163.com>
2021-09-14 21:17:58 +0800

    2021.9.14

--- PowershellScript/gitee.ps1
+++ PowershellScript/gitee.ps1
@@ -1,12 +1,16 @@
-﻿param($a, $b)
+﻿param($a, $b, $c)
+$c = pwd
+$c = $c.Path
 cd D:\v2rayN\
 .\v2rayN.exe
 echo 'Wait a second, Turnning VPN on'
 Start-Sleep -s 1
 echo 'Successful Turning'
+cd "$c"
+$a = git branch
+$a = $a.Substring(2)
 $b = Get-Date -Format 'yyyy.M.d'
 echo "Commit info = $b"
-cd d:\Code\Vscode
 git add .
 git commit -m "$b"
-git push origin NEW
+git push origin "$a"
